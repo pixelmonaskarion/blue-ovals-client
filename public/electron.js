3140c7a6952464f5c4f6480dd6c3fac584093800
Chistopher Huntwork <christopher@huntwork.net>
2023-09-21 21:04:02 -0700

    auth works, no frontend

--- public/electron.js
+++ public/electron.js
@@ -1,76 +1,88 @@
 const electron = require("electron");
-const {Notification, Menu, Tray } = require("electron");
+const { Notification, Menu, Tray } = require("electron");
 const path = require("path");
 const remote_main = require('@electron/remote/main');
 var protobuf = require("protobufjs");
-const { clipboard } = require('electron');
 remote_main.initialize();
-const {WebSocket} = require("ws");
+const { WebSocket } = require("ws");
 const { existsSync, writeFileSync, readFileSync } = require("fs");
-const Crypto = require("./Crypto");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
 let mainWindow;
+let auth;
 
 function startWebsocket() {
-  //We will need this later
-  // const ws = new WebSocket('wss://chrissytopher.com:40441/events/'+auth.email);
+	//We will need this later
+	const ws = new WebSocket('wss://chrissytopher.com:40441/events/' + auth.uuid);
 
-  // ws.on('error', console.error);
+	ws.on('error', console.error);
 
-  // ws.on('open', function open() {
-  //   console.log("ws started");
-  // });
+	ws.on('open', function open() {
+		console.log("ws started");
+		ws.send(auth.email);
+		ws.send(auth.password);
+	});
 
-  // ws.on('message', async function message(data) {
-    
-  // });
+	ws.on('message', async function message(data) {
+
+	});
 }
 
 //Don't call this rn but we will be using protobufs, don't question me on this one
 async function load_protobufs() {
-  return new Promise((resolve, reject) => {
-    protobuf.load("/home/christopher/AndroidStudioProjects/shared-clipboard/shared-clipboard-electron-react/public/shared_clipboard.proto", function (err, root) {
-      if (err) {
-        reject(err);
-      }
-      resolve(root);
-    });
-  })
+	return new Promise((resolve, reject) => {
+		protobuf.load("/home/christopher/AndroidStudioProjects/shared-clipboard/shared-clipboard-electron-react/public/shared_clipboard.proto", function (err, root) {
+			if (err) {
+				reject(err);
+			}
+			resolve(root);
+		});
+	})
 }
 
 let tray = null
 async function createWindow() {
-  //Trays are cool right?
-  if (process.platform === "darwin") {
-    tray = new Tray(electron.nativeImage.createFromPath(__dirname + "/../build/SharedClipboardIcon.png"));
-  } else if (process.platform === "win32") {
-    tray = new Tray(__dirname + "/../build/SharedClipboardColor.ico");
-  } else {
-    tray = new Tray(__dirname + "/../build/SharedClipboardColor.png");
-  }
-  const contextMenu = Menu.buildFromTemplate([
-    { label: 'SharedClipboard', type: 'normal', enabled: false },
-    { label: 'Quit', type: 'checkbox', role: "quit"},
-  ])
-  tray.setContextMenu(contextMenu)
+	//Trays are cool right?
+	if (process.platform === "darwin") {
+		tray = new Tray(electron.nativeImage.createFromPath(__dirname + "/../build/SharedClipboardIcon.png"));
+	} else if (process.platform === "win32") {
+		tray = new Tray(__dirname + "/../build/SharedClipboardColor.ico");
+	} else {
+		tray = new Tray(__dirname + "/../build/SharedClipboardColor.png");
+	}
+	const contextMenu = Menu.buildFromTemplate([
+		{ label: 'SharedClipboard', type: 'normal', enabled: false },
+		{ label: 'Quit', type: 'checkbox', role: "quit" },
+	])
+	tray.setContextMenu(contextMenu)
 
-  // protos = await load_protobufs();
+	// protos = await load_protobufs();
+	if (!existsSync(app.getPath("userData") + "/auth.json")) {
+		mainWindow = new BrowserWindow({
+			width: 800,
+			height: 600,
+			webPreferences: { nodeIntegration: true, contextIsolation: false },
+		});
+		mainWindow.loadFile(path.join(__dirname, "../build/index.html"));
+		let webContents = mainWindow.webContents;
+		remote_main.enable(webContents)
+	} else {
+		auth = JSON.parse(readFileSync(app.getPath("userData") + "/auth.json"));
+		afterAuth();
+	}
 
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: { nodeIntegration: true, contextIsolation: false },
-  });
-  mainWindow.loadFile(path.join(__dirname, "../build/index.html"));
-  let webContents = mainWindow.webContents;
-  remote_main.enable(webContents)
+	
 }
 
-electron.ipcMain.handle('save-auth', async (event, auth) => {
-  //we will probably use this later?
-  writeFileSync(app.getPath("userData")+"/auth.json", JSON.stringify(auth));
+function afterAuth() {
+	startWebsocket();
+}
+
+electron.ipcMain.handle('save-auth', async (event, new_auth) => {
+		auth = new_auth;
+        writeFileSync(app.getPath("userData") + "/auth.json", JSON.stringify(auth));
+        afterAuth();
 });
 
 // This method will be called when Electron has finished
--- src/App.js
+++ src/App.js
@@ -1,6 +1,49 @@
 import './App.css';
+import { useEffect } from 'react';
+import Crypto from "./Crypto.js"
+const Electron = require("electron");
 
 function App() {
+  useEffect(() => {
+    ( async () => {
+      let basic_auth_json = {
+        email: "christopher@huntwork.net",
+        password: "balls",
+      };
+      //check if req failed later but for now it doesn't matter
+      // eslint-disable-next-line no-unused-vars
+      let create_res = await (await fetch("https://chrissytopher.com:40441/create-account/", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(basic_auth_json)
+      })).json();
+      let keys = await Crypto.generateKeyPair();
+      let ids_res = await (await fetch("https://chrissytopher.com:40441/register-ids/", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          ...basic_auth_json,
+          public_key: keys.publicKey,
+        })
+      })).json();
+      if (ids_res.success) {
+        let uuid = ids_res.uuid;
+        let full_auth_json = {
+          ...basic_auth_json,
+          uuid: uuid,
+          public_key: keys.publicKey,
+          private_key: keys.privateKey,
+        };
+        Electron.ipcRenderer.invoke('save-auth', full_auth_json);
+      } else {
+        throw ids_res.error;
+      }
+    })();
+  }, []);
   return (
     <div className="App">
       <h1>Hi Ben you should do some ui 👍️👍️</h1>
